600066aac600810d48c49ab219a80b14f4cf4749
Antonio Rodriguez <antoniwan@gmail.com>
2022-07-09 00:22:12 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
