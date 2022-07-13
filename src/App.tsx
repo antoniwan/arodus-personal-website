62119c4e0458c7d6d0c314593eb2618c3a4a72a5
Antonio Rodriguez <antoniwan@gmail.com>
2022-07-13 10:25:11 -0400

    Add a quick & dirty Google Analytics Page Tracker component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import ReactGA from "react-ga";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import GlobalStylesheet from "./components/GlobalStyles";
 import Home from "./pages/Home";
+
+function GoogleAnalyticsPageTracker({ children }: any) {
+  // This is dirty AF isn't it?
+  const GOOGLE_ANALYTICS_TRACKING_CODE = "UA-49026829-1";
+  const location = useLocation();
+  ReactGA.initialize(GOOGLE_ANALYTICS_TRACKING_CODE);
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [location]);
+  return <></>;
+}
 
 function App() {
   const theme = {
@@ -11,6 +25,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <GoogleAnalyticsPageTracker />
       <GlobalStylesheet />
       <Routes>
         <Route index element={<Home />} />
