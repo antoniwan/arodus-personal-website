c99f2a7be9f003f5bf930756f558a7fa06bbd7b2
Antonio Rodriguez <antoniwan@gmail.com>
2022-07-16 14:51:12 -0400

    Update Container.tsx

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,6 +4,13 @@ const Section = styled.section`
   max-width: var(--max-container-width);
   padding-left: 2rem;
   padding-right: 2rem;
+  margin: 0 auto;
+  width: calc(100% - (2rem * 2));
+
+  @media screen and (min-width: 1024px) {
+    margin-left: unset;
+    margin-right: unset;
+  }
 
   @media screen and (min-width: 960px) {
     padding-left: 4rem;
