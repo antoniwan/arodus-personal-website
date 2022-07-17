b2c61104961b00f8f30abfc5cbc64d41ef96317e
Antonio Rodriguez <antoniwan@gmail.com>
2022-07-16 20:55:07 -0400

    Update Links.tsx

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,16 +1,25 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const StyledLinks = styled.ul`
   display: flex;
   flex-grow: 1;
+  align-items: center;
+  justify-content: flex-start;
+  margin: 0;
+  padding: 0;
+  margin-left: 1rem;
+
+  li {
+    margin: 0;
+    list-style-type: none;
+  }
 
   a:link,
   a:active,
   a:visited {
     display: block;
-    padding: 2rem;
+    padding: 10px;
     color: var(--color-white);
     text-shadow: 1px 1px 0px rgba(0, 0, 0, 0.2);
     font-size: 1.5rem;
@@ -19,7 +28,8 @@ const StyledLinks = styled.ul`
     transition: all 0.5s ease;
 
     &:hover {
-      text-shadow: 1px 1px 0px rgba(0, 0, 0, 1);
+      color: var(--color-black);
+      text-shadow: -1px -1px 0px rgba(255, 255, 255, 0.5);
     }
 
     &.active {
@@ -30,31 +40,14 @@ const StyledLinks = styled.ul`
 
   @media (min-width: 1024px) {
     flex-direction: column;
-    padding-bottom: 0;
+    margin: 0;
+    padding-top: 2rem;
 
-    li {
-      margin-top: 1rem;
-    }
-    a {
-      padding: 0;
-      padding-right: 1rem;
-
+    a:link,
+    a:active,
+    a:visited {
       font-size: 2rem;
-      line-height: 1;
-      text-decoration: none;
-      text-shadow: none;
-      text-align: center;
-      &:hover {
-        color: var(--color-black);
-      }
-
-      &.active {
-        font-weight: normal;
-        border-bottom: none;
-        border: none;
-        color: var(--color-pink);
-        transition: all 0.2s;
-      }
+      line-height: 2rem;
     }
   }
 `;
