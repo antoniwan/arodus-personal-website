3984b580944759e0471116337f26c81b7ea8a9ef
Antonio Rodriguez <antoniwan@gmail.com>
2022-07-16 20:54:53 -0400

    Update Container.tsx

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,21 +2,14 @@ import styled from "styled-components";
 
 const Section = styled.section`
   max-width: var(--max-container-width);
-  padding-left: 2rem;
-  padding-right: 2rem;
+  padding-left: 4rem;
+  padding-right: 4rem;
   margin: 0 auto;
-  width: calc(100% - (2rem * 2));
+  width: calc(100% - (4rem * 2));
 
   @media screen and (min-width: 1024px) {
     margin-left: unset;
     margin-right: unset;
-  }
-
-  @media screen and (min-width: 960px) {
-    padding-left: 4rem;
-    padding-right: 2rem;
-    padding-top: 2rem;
-    padding-bottom: 4rem;
   }
 `;
 
