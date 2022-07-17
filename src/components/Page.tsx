300856c3bc4fb22688b877bdab1ae136391c85a1
Antonio Rodriguez <antoniwan@gmail.com>
2022-07-16 20:55:14 -0400

    Update Page.tsx

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -5,34 +5,9 @@ const StyledPage = styled.div`
   flex-direction: column;
   align-items: flex-start;
 
-  p {
-    text-align: left;
-  }
-
-  img {
-    height: 300px;
-    width: 300px;
-    object-fit: cover;
-    margin-bottom: 5rem;
-    border-radius: 300px;
-    margin-top: 1rem;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-  }
-
   .discus-embed {
     width: 100%;
     margin-top: 4rem;
-  }
-
-  @media (min-width: 1024px) {
-    img {
-      margin-top: 0;
-      border-radius: 300px;
-      height: 300px;
-      width: 300px;
-    }
   }
 `;
 
