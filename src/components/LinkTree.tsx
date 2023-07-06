c7dfb5a5d0461fc8ba93c1d737af1c0504493885
Antonio R <antoniwan@icloud.com>
2023-07-06 17:57:16 -0400

    Update LinkTree.tsx

--- src/components/LinkTree.tsx
+++ src/components/LinkTree.tsx
@@ -11,7 +11,7 @@ export default function LinkTree() {
     <StyledLinkTree>
       <li>
         <a
-          href="https://www.linkedin.com/in/antoniwan/"
+          href="https://www.linkedin.com/in/antonioysusoftware/"
           rel="noopener no noreferrer"
           target="_blank"
         >
@@ -29,7 +29,7 @@ export default function LinkTree() {
       </li>
       <li>
         <a
-          href="https://codepen.io/antoniwan/#"
+          href="https://codepen.io/antonio666917/#"
           rel="noopener no noreferrer"
           target="_blank"
         >
