e697b8ec4533cdbb3a920943d3136c7ea354a270
Antonio Rodriguez <antoniwan@icloud.com>
2024-02-09 11:52:26 -0500

    Adds a new project!

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -8,6 +8,8 @@ import Ebook1Picture from "../images/projects/EBook-MiaTheSunTheMoon.jpg";
 import Ebook1Picture2x from "../images/projects/EBook-MiaTheSunTheMoon.jpg";
 import JCPicture from "../images/projects/JCPortfolio.jpg";
 import JCPicture2x from "../images/projects/JCPortfolio.jpg";
+import WordleClonePicture from "../images/projects/WordleClone.png";
+import WordleClonePicture2x from "../images/projects/WordleClone.png";
 
 const StyledProjectList = styled.ul`
   display: flex;
@@ -76,12 +78,70 @@ export default function Projects({
             </h2>
             <h3>Children's E-book in English and Spanish</h3>
             <p>
-              I used the create-react-app React starter to create a simple
-              e-book website. It uses Styled Components, basic state hooks, and
-              local storage to save the chosen language and the last page the
-              user browsed. I made the illustrations using multiple AI
-              generators, and writing the content was the most challenging part.
-              The design is entirely responsive.
+              I used the{" "}
+              <a
+                href="https://create-react-app.dev"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                create-react-app
+              </a>{" "}
+              React starter to create a simple e-book website. It uses{" "}
+              <a
+                href="https://styled-components.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Styled Components
+              </a>
+              , React hooks, and context to manage state and local storage to
+              save the chosen language and the last page the user browsed. I
+              made the illustrations using multiple AI generators, and writing
+              the content was the most challenging part. The design is simple,
+              although entirely responsive.
+            </p>
+          </StyledProjectListItem>
+
+          <StyledProjectListItem>
+            <img
+              srcSet={`${WordleClonePicture} 1x, ${WordleClonePicture2x} 2x`}
+              alt="Wordle Game Clone Screenshot"
+            />
+
+            <h2>
+              <a
+                href="https://wordle-clone-arm.netlify.app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Word Game - Practice Exercise
+              </a>
+            </h2>
+            <h3>Web-Based Wordle Game Clone</h3>
+            <p>
+              This Wordle word game clone is a practice exercise from{" "}
+              <a
+                href="https://www.joyofreact.com"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                The Joy of React
+              </a>{" "}
+              by <em>Josh W Comeau</em>, and it had me practice creating dynamic
+              user interfaces and bundling the React web application with{" "}
+              <a
+                href="https://parceljs.org"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Parcel's
+              </a>{" "}
+              minimum build tooling. Throughout the exercise, I practiced React
+              hooks, flexbox CSS, data bindings, form submissions, regular
+              expressions, state hooks, cool JavaScript syntax tricks (object
+              destructuring, spread operators, others), utility 'library' files
+              organization, conditional rendering, event handlers, and other
+              goodies. Please give it a spin; it should be fun!
             </p>
           </StyledProjectListItem>
 
@@ -102,12 +162,27 @@ export default function Projects({
             </h2>
             <h3>Personal Portfolio</h3>
             <p>
-              I used the create-react-app React starter to create a front-end
-              heavy portfolio website. My friend designed it, and I created the
-              components, layout, breakpoints, and necessary JavaScript and
-              React hooks to calculate and animate all transitions and particles
-              using the Framer animation libraries for mobile, tablet, and
-              desktop breakpoints - this was particularly challenging.
+              I used the{" "}
+              <a
+                href="https://create-react-app.dev"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                create-react-app
+              </a>{" "}
+              React starter to create a front-end heavy portfolio website for my
+              Googler friend who designed it. I developed the components,
+              layouts, and necessary JavaScript and React hooks to calculate and
+              animate all transitions and particle effects using the{" "}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.framer.com/motion/animation/"
+              >
+                Framer Motion
+              </a>{" "}
+              animation libraries for mobile, tablet, and desktop breakpoints -
+              this was particularly challenging.
             </p>
           </StyledProjectListItem>
         </StyledProjectList>
